db22bf9501de5d2517de7576ec3331cc23f6dd8e
SoumyaRanjan63 <130931666+SoumyaRanjan63@users.noreply.github.com>
2023-06-22 20:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function getRandomTime(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createPromiseWithRandomTime() {
+  return new Promise((resolve) => {
+    const randomTime = getRandomTime(1000, 5000);
+    setTimeout(() => {
+      resolve(randomTime);
+    }, randomTime);
+  });
+}
+
+const promises = Array.from({ length: 5 }, createPromiseWithRandomTime);
+
+Promise.any(promises)
+  .then((result) => {
+    document.getElementById("output").textContent = result;
+  })
+  .catch((error) => {
+    console.error("Error occurred:", error);
+  });
+
